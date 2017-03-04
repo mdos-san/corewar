b8a38a20c0afa0a7684cc20cc1a7146347363eda
Mickael DOS SANTOS <mdos-san@e2r11p10.42.fr>
2017-03-04 14:14:16 +0100

    fix lfork

--- srcs/fork.c
+++ srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 14:33:35 by mdos-san          #+#    #+#             */
-/*   Updated: 2017/03/03 14:33:58 by mdos-san         ###   ########.fr       */
+/*   Updated: 2017/03/04 14:13:36 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,22 @@ void	frk(t_cw *cw, t_process *p)
 
 void	lfrk(t_cw *cw, t_process *p)
 {
-	unsigned short	j;
-	t_process		*child;
-	int				i;
+	short		j;
+	t_process	*child;
+	int			i;
 
 	i = 0;
 	j = 0;
 	((unsigned char *)&j)[1] = cw->board[mod(p->pc, 1)];
 	((unsigned char *)&j)[0] = cw->board[mod(p->pc, 2)];
-	j = (int)(short)(j);
-	child = process_new(&cw->process, p->nb_champ, mod(p->pc, j), p->color_nb);
+	child = process_new(&cw->process, p->nb_champ,
+			mod(p->pc, j), p->color_nb);
 	while (i < 16)
 	{
 		child->r[i] = p->r[i];
 		++i;
 	}
 	child->carry = p->carry;
-	child->nb_live = 1;
+	child->nb_live = p->nb_live;
 	cw->nb_readed = 3;
 }
